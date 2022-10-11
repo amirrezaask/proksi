2510172f4e66258f08a012cdb3168f8a0e50c1fb
amirrezaask <raskarpour@gmail.com>
2022-10-11 20:43:15 +0330

    using hyper

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,49 @@
-use actix_web::{route, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use std::ops::Deref;
+use hyper::http::HeaderValue;
+use hyper::{Body, Request, Response, Server, Client};
+use hyper::service::{make_service_fn, service_fn};
+use serde::{Deserialize, Serialize};
+use anyhow::Result;
 
-// fn create_request(_req: HttpRequest, client: reqwest::Client) -> reqwest::Request {
-//     client.request(_req.method, )
-// }
-#[route(
-    "/",
-    method = "GET",
-    method = "POST",
-    method = "PUT",
-    method = "PATCH",
-    method = "DELETE",
-    method = "OPTIONS",
-    method = "HEAD",
-    method = "CONNECT",
-    method = "TRACE"
-)]
-
-async fn proksi_bridge(_req: HttpRequest) -> impl Responder {
-    dbg!(_req);
-    HttpResponse::Ok()
+// const UPSTREAM: &'static str = "http://localhost:8080";  
+async fn bridge(mut req: Request<Body>) -> Result<Response<Body>> {
+    println!("bridge...");
+    let client = Client::new();
+    let actual_host = req.headers().get("HOST").unwrap().clone();
+    req.headers_mut().append("PROKSI-REAL-HOST", actual_host.clone());
+    dbg!(req.uri());
+    req.headers_mut().insert("HOST", HeaderValue::from_str("localhost:8080").unwrap());
+    let mut resp = client.request(req).await?;
+    Ok(Response::new("Hello, World".into()))
 }
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(proksi_bridge))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+async fn upstream(mut req: Request<Body>) -> Result<Response<Body>> {
+    println!("upstream...");
+    dbg!(req.uri());
+    return Ok(Response::new("Hello, World".into()))
+}
+async fn handler(req: Request<Body>) -> Result<Response<Body>> {
+    if req.uri().path().contains("upstream") {
+       upstream(req).await
+    } else {
+        bridge(req).await
+    }
+    
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, anyhow::Error>(service_fn(handler))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
 }
